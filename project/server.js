aa29220cee9ea80ae910cac7d014ae9e8559a4e7
Mari <mari@Maris-Air.lan>
2021-12-11 21:22:52 +0200

    Add a new post

--- project/server.js
+++ project/server.js
@@ -18,3 +18,15 @@ app.get('/posts', async(req, res) => {
         console.error(err.message);
     }
 });
+
+app.post('/posts/', async(req, res) => {
+    try {
+        console.log("a post request has arrived");
+        const post = req.body;
+        const newpost = await pool.query(
+            "INSERT INTO nodetable(title, body, urllink) values ($1, $2, $3) RETURNING*", [post.title, post.body, post.urllink]);
+        res.json( newpost );
+    } catch (err) {
+        console.error(err.message);
+    }
+});
