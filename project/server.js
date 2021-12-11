d21dfbc8762e8a6c99c71b925adc4f8b4d671fe4
Mari <mari@Maris-Air.lan>
2021-12-11 21:17:07 +0200

    Retrieve all posts

--- project/server.js
+++ project/server.js
@@ -6,3 +6,15 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log("Server is listening to port 3000")
 });
+
+app.get('/posts', async(req, res) => {
+    try {
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM nodetable"
+        );
+        res.json(posts.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
